948629bbe5a5956db1fe1752780655e516a31744
Terra Miller <tmiller1142.dev@gmail.com>
2021-10-06 06:20:11 +0000

    Added Kafka Producer creation and enhancing data postback objects with endpoint method/url and starting timestamp

--- ingest.php
+++ ingest.php
@@ -1,4 +1,8 @@
 <?php
+require '/home/kafka/kafka/vendor/autoload.php';
+use Kafka\Protocol;
+use Kafka\Socket;
+date_default_timezone_set('UTC');
 
 // Verify that HTTP request method is POST
 if(strcasecmp($_SERVER['REQUEST_METHOD'], 'POST') != 0) {
@@ -20,23 +24,36 @@ $json = file_get_contents('php://input');
 $decoded = json_decode($json);
 
 // Verify request payload is was valid JSON
-if(!is_array($data)){
+if(!is_array($decoded)){
     echo 'Data not in JSON format';
 }
 
-echo $decoded->endpoint->method;
-echo "\n";
-echo $decoded->endpoint->url;
-echo "\n";
+// /Set up Kafka Producer 
+$config = \Kafka\ProducerConfig::getInstance();
+$config->setMetadataRefreshIntervalMs(10000);
+$config->setMetadataBrokerList('127.0.0.1:9092');
+$config->setBrokerVersion('1.0.0');
+$config->setRequiredAck(1);
+$config->setIsAsyn(false);
+$config->setProduceInterval(500);
+$producer = new \Kafka\Producer();
 
-$data = $decoded->data;
-echo $data;
-
-// Cycle over data objects
+// Send data postback object for each object in $data
+// Add endpoint method, url and timestamp
 foreach($data as $item) {
     var_dump($item);
-    echo $item->mascot;
-    echo "\n";
+    $now_time = date("Y.m.d H:i:s");
+    $item->endpoint_method = $decoded->endpoint->method;
+    $item->endpoint_url = $decoded->endpoint->url;
+    $item->start_time = $now_time;
+    
+    $producer->send([
+        [
+            'topic' => 'postback.delivery',
+            'value' => json_encode($item),
+            'key' => '',
+        ],
+    ]);
 }
 
 ?>
